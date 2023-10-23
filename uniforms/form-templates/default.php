878b6bb20f259a0049c8b98584aff3768e6ef583
UNI-Studio <design@uni-studio.ru>
2023-10-23 15:44:32 +0300

    Добавлен блок согласия в форму

--- uniforms/form-templates/default.php
+++ uniforms/form-templates/default.php
@@ -23,6 +23,12 @@
                     <input type="email" name="email" class="uniforms__field-group__input" id="lbEmail" placeholder="Электронная почта" value="">
                     <div class="uniforms__field-group__error"></div>                   
                 </div>
+
+                <div class="uniforms__agremeent">
+                    <input required="" name="pd" checked="checked" autocomplite="off" type="checkbox" class="uniforms__agremeent__checkbox">
+                    <p class="uniforms__agremeent__text">Я даю свое <a href="/consent-processing-personal/" target="_blank" rel="noopener">согласие на обработку моих персональных данных</a>.</p>
+                </div>
+
                 <div class="uniforms__submit-block">
                     <button class="uniforms__button-submit" type="submit">Отправить</button>
                 </div>
